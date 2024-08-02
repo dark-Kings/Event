e4cb8cf40a0ef8df14d75a71f4fe6aa8c50a9b53
Anshul Pant <anshulpant14@gmail.com>
2024-08-02 18:28:29 +0530

    gift feature added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import HomeComponent from './component/HomeComponent';
 import Login from './component/Login';
 import Footer from './component/Footer';
+import Error from './component/Error';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/home" element={<HomeComponent />} />
+            <Route path='*' element={<Error />} />
 
           </Routes>
 
